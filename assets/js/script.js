9e908ff73e2987693becc137de6bbb5de4bc8d55
becket <becket.bowes@gmail.com>
2022-01-22 14:58:53 -0500

    fully functional mvp, with tasks saving and colors a changin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,63 @@
-var schedNum = 0;
-const wholeSched = {};
+var setStorage = function() {
+    var wholeSched = [["5am", "wakeup"]];
+    localStorage.setItem("oldSched", JSON.stringify(wholeSched));
+};
+// setStorage();
+var newOldStorage = localStorage.getItem("oldSched");
+if (newOldStorage == undefined) {
+    setStorage();
+    newOldStorage = localStorage.getItem("oldSched");
+};
+var wholeSched = JSON.parse(newOldStorage);
+var wholeSchedLength = Object.keys(wholeSched).length;
+var schedNum = (wholeSchedLength);
 
 moment().hour();
 moment().day();
 moment().date();
 moment().month();
 moment().year();
+var hour = moment().format("ha");
+var funkHour = (hour-6)
+console.log(hour)
+
+// loop through with if elseif else "hour" to change class
+var colorator = function() {
+    var flag = false;
+    $(".todo").each(function() {
+        if (flag === false) {
+            $(this).addClass("past");
+        };
+
+        if (flag === true) {
+            $(this).addClass("future");
+        };
+
+        if ($(this).data("time") === hour) {
+            $(this).addClass("present");
+            flag = true;
+        };
+    })
+};
 
 var date = moment().format("dddd, MMMM Do, YYYY");
 $(".currentDay").text(date);
 
-
-
-//TODO fix this so it places remembered tasks in right slots!!!
 //replace tasks
 var replaceTasks = function () {
-    var x = 0
-    var previouslyOn = JSON.parse(localStorage.getItem("oldSched"));
-    previouslyOnLength = Object.keys(previouslyOn).length
-    console.log(previouslyOnLength);
     $(".hour").each(function () {
-        var oldTaskTime = (previouslyOn[x][0]);
-        var oldTask = (previouslyOn[x][1]);
-        var calendarSlot = $(".hour").text();
-        if (oldTaskTime = calendarSlot) {
-            var write = $("<textarea>").addClass("textareaB").addClass("toDoMade").val(oldTask).wrapInner("p");
-            $(this).siblings(".todo").append(write);
-        };
-        // return(!(x<previouslyOnLength));
-        x++;
+        var calendarSlot = $(this).text();
+        for (let i = 0; i < wholeSchedLength; i++) {
+            var oldTaskTime = (wholeSched[i][0]);
+            var oldTask = (wholeSched[i][1]);
+            if (oldTaskTime === calendarSlot) {
+                var write = $("<textarea>").addClass("textareaB").addClass("toDoMade").val(oldTask).wrapInner("p");
+                $(this).siblings(".todo").append(write);
+            };
+        }
     });
 };
 
-//
 // make tasks
 $(".todo").click(function () {
     var write = $("<textarea>").addClass("textareaB").addClass("toDoMade").val("").wrapInner("p");
@@ -53,19 +78,6 @@ $(".saveBtn").click(function () {
     localStorage.setItem("oldSched", JSON.stringify(wholeSched));
 });
 
+colorator();
 replaceTasks();
 
-
-var replaceTasks = function () {
-    var x = 0
-    var previouslyOn = JSON.parse(localStorage.getItem("oldSched"));
-    previouslyOnLength = Object.keys(previouslyOn).length
-    console.log(previouslyOnLength);
-    var oldTaskTime = (previouslyOn[x][0]);
-    var oldTask = (previouslyOn[x][1]);
-    jQuery.each(function( previouslyOnLength, oldTaskTime ) {
-        var write = $("<textarea>").addClass("textareaB").addClass("toDoMade").val(oldTask).wrapInner("p");
-        $("#" + oldTaskTime).siblings(".todo").append(write);
-    });
-    x++
-};
